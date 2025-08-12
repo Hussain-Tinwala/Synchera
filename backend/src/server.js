1682ff38003213b9ffbe8fcc6115d283503e1508
Hussain Tinwala <coder.hussaintin@gmail.com>
2025-08-12 22:50:57 +0530

    feat(db): Set up and connected MongoDB

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import authRoutes from './routes/auth.route.js'
+import { connectDB } from './lib/db.js'
 dotenv.config()
 
 const app = express()
@@ -20,6 +21,7 @@ app.use('/api/auth', authRoutes)
 
 app.listen(PORT, () => {
     console.log(`App is listening on port ${PORT}`)
+    connectDB()
 
     }
 )
