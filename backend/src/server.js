d49877ee22ef8d55ef134cd6889b4a2616454781
Hussain Tinwala <coder.hussaintin@gmail.com>
2025-08-12 22:28:43 +0530

    feat(backend): Setup server, Express router, and auth controllers

---
+++ backend/src/server.js
@@ -0,0 +1,25 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import authRoutes from './routes/auth.route.js'
+dotenv.config()
+
+const app = express()
+const PORT=process.env.PORT
+
+app.use('/api/auth', authRoutes)
+
+// app.get('/api/auth/signup',(req,res)=>{
+//     res.send("sign up ")
+// })
+// app.get('/api/auth/signin',(req,res)=>{
+//     res.send("sign in ")
+// })
+// app.get('/api/auth/logout',(req,res)=>{
+//     res.send("log out ")
+// })
+
+app.listen(PORT, () => {
+    console.log(`App is listening on port ${PORT}`)
+
+    }
+)
