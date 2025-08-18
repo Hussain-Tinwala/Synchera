381f914ffa02c0cff7c7ad41d7a2b57a42c53a3b
Hussain Tinwala <coder.hussaintin@gmail.com>
2025-08-18 21:22:22 +0530

    feat(frontend): Setup TanStack Query and basic page structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,53 @@
 import React from 'react'
+import { Route, Routes } from 'react-router'
+import HomePage from './pages/HomePage'
+import NotificationsPage from './pages/NotificationsPage'
+import CallPage from './pages/CallPage'
+import ChatPage from './pages/ChatPage'
+import OnboardingPage from './pages/OnboardingPage'
+import SignupPage from './pages/SignupPage'
+import LoginPage from './pages/LoginPage'
+
+import toast, {Toaster} from "react-hot-toast"
+
+import { useQuery } from '@tanstack/react-query'
+import axios from "axios"
+import { axiosInstance } from './lib/axios'
 
 const App = () => {
+  const {data, isLoading, error}=useQuery({queryKey: ["todos"],
+    queryFn: async()=>{
+      // const res=await fetch('https://jsonplaceholder.typicode.com/todos')
+      // const data=await res.json()
+      // return data
+
+      const res=await axiosInstance.get("http://localhost:3000/api/auth")
+      return res.data;
+    }
+  })
+
+  console.log(data)
+  console.log({isLoading})
+  console.log({error})
+
   return (
     <>
-      <div  data-theme='forest'>
-        
-      <button className="btn">Button</button>
-      <button className="btn btn-neutral">Neutral</button>
-      <button className="btn btn-primary">Primary</button>
-      <button className="btn btn-secondary">Secondary</button>
-      <button className="btn btn-accent">Accent</button>
-      <button className="btn btn-ghost">Ghost</button>
-      <button className="btn btn-link">Link</button>
-      </div>
+    <div className='h-screen' data-theme="night">
+      {/* <button onClick={()=>toast.error("Hello!")}>Create Toast</button> */}
+      
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/notifications" element={<NotificationsPage />} />
+        <Route path="/call" element={<CallPage />} />
+        <Route path="/chat" element={<ChatPage />} />
+        <Route path="/onboarding" element={<OnboardingPage />} />
+
+      </Routes>
+
+      <Toaster />
+    </div>
     </>
 
   )
