ea832f53d274c4692f38777da79666b5f1db977a
Hussain Tinwala <coder.hussaintin@gmail.com>
2025-08-18 17:51:34 +0530

    feat(users) Implemented Send Friend requests logic

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,4 +1,5 @@
 import User from "../models/User.model.js";
+import FriendRequest from "../models/FriendRequest.model.js";
 
 export const recommendedUsers= async(req, res)=>{
     try {
@@ -41,4 +42,54 @@ export const myFriends= async(req, res)=>{
         
     
 
+}
+
+export const sendFriendRequest=async(req, res)=>{
+    try {
+        const myId=req.user.id;
+        const {id: receieverId}=req.params;
+
+        if(myId===receieverId) return res.status(400).json({message: "You can't send friend request to yourself"})
+        
+        const receiver=await User.findById(receieverId);
+        if(!receiver)
+        {
+            return res.status(404).json({message: "Receiver not Found"})
+
+        }
+        
+        // check if a user is already friend
+        if(receiver.friends.includes(myId))
+        {
+            return res.status(400).json({message: "Already friends"})
+        }
+
+
+        //check if a req already exists
+        const existingRequest= await FriendRequest.findOne({
+            $or:[
+                {sender: myId, receiver: receieverId},
+                {sender: receieverId, receiver: myId},
+            ],
+
+        })
+
+        if(existingRequest)
+        {
+            return res.status(400).json({message: "A friend request already exists between you and this user"})
+        }
+
+        const friendRequest=await FriendRequest.create({
+            sender: myId,
+            receiver: receieverId,
+        })
+
+        res.status(201).json(friendRequest)
+
+        
+    } catch (error) {
+        console.error("Error in sendFriendRequest controller", error.message)
+        res.status(500).json({message: "Internal Server Error"})
+    
+    }
 }
--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,6 +1,6 @@
 import express from "express"
 import { protectRoute } from "../middleware/auth.middleware.js";
-import { myFriends, recommendedUsers } from "../controllers/user.controller.js";
+import { myFriends, recommendedUsers, sendFriendRequest } from "../controllers/user.controller.js";
 
 const router= express.Router();
 
@@ -8,5 +8,8 @@ router.use(protectRoute) //.. applies auth middleware to all the router below
 
 router.get("/",recommendedUsers);
 router.get("/friends", myFriends);
+
+router.post("/friend-request/:id", sendFriendRequest);
+
 
 export default router;
